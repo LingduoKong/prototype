490012c6219ad3be05d9f42f0f5cd4c879290b58
lingduokong <lingduokong@gmail.com>
2015-06-25 13:22:03 -0500

    finish funstion of moving cards to another list

--- trello-practice.rb
+++ trello-practice.rb
@@ -9,37 +9,42 @@
 #   config.member_token = '61a0f93b284263b4439a0ee064a82bc09bc6df125978a34891792026cb3b90e7'
 # end
 # 2ee3ba64087c31a8f85af0aa482315bc1025091086517f1bdd9eea1038cfe0fc
+
 require 'trello'
 
 TRELLO_DEVELOPER_PUBLIC_KEY="7e9522e062d8095332b4a1abea9d5c2d"
 
-TRELLO_MEMBER_TOKEN=
+TRELLO_MEMBER_TOKEN="2ee3ba64087c31a8f85af0aa482315bc1025091086517f1bdd9eea1038cfe0fc"
 
 Trello.configure do |trello|
   trello.developer_public_key = TRELLO_DEVELOPER_PUBLIC_KEY
   trello.member_token = TRELLO_MEMBER_TOKEN
 end
 
+# test: 558b0e12f2e6f4aa751abe4d
+# dev: 552454f0fc2cbc53c307b025
+
 def get_board
-  @board = Trello::Board.find("552454f0fc2cbc53c307b025")
-  @board
+  @board = Trello::Board.find("558b0e12f2e6f4aa751abe4d")
+  return @board
 end
 
 def get_lists
   @lists = @board.lists
+  return @lists
 end
 
 def get_list(list_name)
   @lists.each do |list|
     if list.name[list_name]
       @list = list
-      p @list
+      return @list
     end
   end
 end
 
 def get_cards(list)
-  @cards= list.cards
+  return @cards= list.cards
 end
 
 def get_checklist_item(issue_number)
@@ -66,10 +71,19 @@ def change_item_state(checklist, item)
   checklist.save
 end
 
+def move_cards(list_from_name, list_to_name)
+  from_list = get_list(list_from_name)
+  to_list = get_list(list_to_name)
+  cards = get_cards(from_list)
+  cards.each do |card|
+   card.move_to_list(to_list)
+  end
+end
+
 get_board
 get_lists
-get_list("Deployment to Staging")
-get_cards(@list)
+# get_list("Deployment to Staging")
+# get_cards(@list)
 
 
 
